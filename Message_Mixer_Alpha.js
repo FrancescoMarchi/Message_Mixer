450477be7ab173b3f034929425f676b8d9d2d7fb
Francesco <marchi.frncsc@gmail.com>
2021-07-26 18:12:52 +0200

    Add randomizer for every array

--- Message_Mixer_Alpha.js
+++ Message_Mixer_Alpha.js
@@ -1,4 +1,5 @@
 const people = ['Louis', 'Anthon', 'Sebastian', 'Lucille', 'Cara', 'Zhang', 'You'];
+randomPeople = people[Math.floor(Math.random() * people.length)];
 
 const places = [
 	'on an italian balcony',
@@ -8,8 +9,10 @@ const places = [
 	'in a rest home',
 	'at the circus',
 ];
+randomPlaces = places[Math.floor(Math.random() * places.length)];
 
 const people2 = ['Anna', 'Dong', 'Friedrick', 'Dimitri', 'Pablo', 'Pedro', 'your crush'];
+randomPeople2 = people2[Math.floor(Math.random() * people2.length)];
 
 const actions = [
 	'fight over who can pee farther',
@@ -19,3 +22,4 @@ const actions = [
 	'invoke Santa to know whether they are good guys',
 	'begin their search for the dragonballs',
 ];
+randomActions = actions[Math.floor(Math.random() * actions.length)];
