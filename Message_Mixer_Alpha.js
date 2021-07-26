fd23bc3f3faa59065e4d39e2a1902d4e872a24f7
Francesco <marchi.frncsc@gmail.com>
2021-07-26 18:11:20 +0200

    Add people, poeple2, places and actions

---
+++ Message_Mixer_Alpha.js
@@ -0,0 +1,21 @@
+const people = ['Louis', 'Anthon', 'Sebastian', 'Lucille', 'Cara', 'Zhang', 'You'];
+
+const places = [
+	'on an italian balcony',
+	'at the base of Kilimanjaro',
+	'in Madagascar',
+	"in Amsterdam's red light district",
+	'in a rest home',
+	'at the circus',
+];
+
+const people2 = ['Anna', 'Dong', 'Friedrick', 'Dimitri', 'Pablo', 'Pedro', 'your crush'];
+
+const actions = [
+	'fight over who can pee farther',
+	'taste glory',
+	'experience mystic realities',
+	'discuss wether E.T. was sexy',
+	'invoke Santa to know whether they are good guys',
+	'begin their search for the dragonballs',
+];
